8ec688e5d56620a3f81ad83bea8d5de7ba01e0ad
k2rosvall <k2.rosas.v@gmail.com>
2020-11-12 14:59:19 -0600

    Generate sequence of numbers

--- javascript/script.js
+++ javascript/script.js
@@ -1,4 +1,3 @@
-
 const startButton = document.getElementById("btnStart");
 const blueButton = document.getElementById("blue");
 const redButton = document.getElementById("red");
@@ -6,19 +5,30 @@ const yellowButton = document.getElementById("yellow");
 const greenButton = document.getElementById("green");
 
 class Game {
-    constructor(){
-        this.initialize();
-    }
+  constructor() {
+    this.initialize();
+    this.generateSequence();
+  }
 
-    initialize() {
-        startButton.classList.add("hide");
+  initialize() {
+    startButton.classList.add("hide");
+    this.level = 1;
+    this.colors = {
+        blue : blueButton,
+        red : redButton,
+        yellow : yellowButton,
+        green: greenButton,
     }
+  }
+
+  generateSequence() {
+    this.sequence = new Array(10).fill(0).map( n => Math.floor(Math.random() * 4));
+  }
 }
 
 startButton.onclick = startGame;
 
 function startGame() {
-    //alert("The game is starting");
-    var game = new Game();
+  //alert("The game is starting");
+  window.game = new Game();
 }
-
