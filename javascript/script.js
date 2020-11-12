003131df23a8a9775af3a2736649155f9b702db2
k2rosvall <k2.rosas.v@gmail.com>
2020-11-12 17:24:52 -0600

    verify chosen color

--- javascript/script.js
+++ javascript/script.js
@@ -3,12 +3,13 @@ const blueButton = document.getElementById("blue");
 const redButton = document.getElementById("red");
 const yellowButton = document.getElementById("yellow");
 const greenButton = document.getElementById("green");
+const LAST_LEVEL = 3;
 
 class Game {
   constructor() {
     this.initialize();
     this.generateSequence();
-    this.nextLevel();
+    setTimeout(this.nextLevel(), 500);
   }
 
   /*
@@ -16,9 +17,13 @@ class Game {
     game we created in startGame.
     If we don't use bind(), the 'this' in the function chooseColor() is 
     actually the 'div' element in the HTML
+
+    If we don't use bind(), the 'this' in the function nextLevel() is
+    actually the window, because setTimeout() is the one calling nextLevel
   
   */
   initialize() {
+    this.nextLevel = this.nextLevel.bind(this);
     this.chooseColor = this.chooseColor.bind(this);
     startButton.classList.add("hide");
     this.level = 1;
@@ -31,17 +36,18 @@ class Game {
   }
 
   generateSequence() {
-    this.sequence = new Array(10)
+    this.sequence = new Array(LAST_LEVEL)
       .fill(0)
       .map((n) => Math.floor(Math.random() * 4));
   }
 
   nextLevel() {
+    this.subLevel = 0;
     this.turnOnSequence();
     this.addClickEvents();
   }
 
-  convertNameToColor(number) {
+  convertNumberToColor(number) {
     switch (number) {
       case 0:
         return "blue";
@@ -54,9 +60,22 @@ class Game {
     }
   }
 
+  convertColorToNumber(color) {
+    switch (color) {
+      case "blue":
+        return 0;
+      case "red":
+        return 1;
+      case "yellow":
+        return 2;
+      case "green":
+        return 3;
+    }
+  }
+
   turnOnSequence() {
     for (let i = 0; i < this.level; i++) {
-      const color = this.convertNameToColor(this.sequence[i]);
+      const color = this.convertNumberToColor(this.sequence[i]);
       setTimeout(() => this.turnOnColor(color), 1000 * i);
     }
   }
@@ -77,8 +96,30 @@ class Game {
     this.colors.green.addEventListener("click", this.chooseColor);
   }
 
+  removeClickEvents() {
+    this.colors.blue.removeEventListener("click", this.chooseColor);
+    this.colors.red.removeEventListener("click", this.chooseColor);
+    this.colors.yellow.removeEventListener("click", this.chooseColor);
+    this.colors.green.removeEventListener("click", this.chooseColor);
+  }
   chooseColor(ev) {
-    console.log(this);
+    const nameColor = ev.target.dataset.color;
+    const numberColor = this.convertColorToNumber(nameColor);
+    this.turnOnColor(nameColor);
+    if (numberColor === this.sequence[this.subLevel]) {
+      this.subLevel++;
+      if (this.subLevel === this.level) {
+        this.level++;
+        this.removeClickEvents();
+        if (this.level === LAST_LEVEL + 1) {
+          //win
+        } else {
+          setTimeout(this.nextLevel, 1500);
+        }
+      }
+    } else {
+      //lost
+    }
   }
 }
 
